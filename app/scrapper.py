bf1bb82f22384d9cf7dbc9c3a3542dfe6740e650
Iván <72264031+xIvqn@users.noreply.github.com>
2025-02-06 11:44:39 +0100

    fix: convert numbers

--- app/scrapper.py
+++ app/scrapper.py
@@ -26,11 +26,11 @@ class Scrapper:
             tds = row.find_all('td')
             if len(tds) == 5:
                 users.append({
-                    'rank': tds[0].text,
+                    'rank': int(tds[0].text),
                     'user': tds[1].find('a').text,
                     'country': None if tds[2].find('a') is None else tds[2].find('a')['title'],
                     'subdivision': None if tds[3].find('a') is None else tds[3].find('a')['title'],
-                    'score': tds[4].text,
+                    'score':  float(tds[4].text),
                     'nickname': tds[1].find('a')['href'].split('/')[-1],
                 })
             Logger.log(f'Processed {users[-1]["nickname"]}')
