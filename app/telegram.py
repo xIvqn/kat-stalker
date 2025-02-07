9b70b0846ed4d389bffafba2351d9faac9f83918
Iván <72264031+xIvqn@users.noreply.github.com>
2025-02-07 18:52:43 +0100

    fix: escape underscores

--- app/telegram.py
+++ app/telegram.py
@@ -1,4 +1,5 @@
 import os
+import sys
 from html import escape
 
 import requests
@@ -9,8 +10,14 @@ class Telegram:
         self.token = os.environ['TELEGRAM_TOKEN']
 
     def send(self, chat_id, message):
-        message = escape(message).replace('.', '\\.').replace('-', '\\-')
+        if chat_id == "":
+            print("Skipped empty chat_id")
+            return
+        message = escape(message)\
+            .replace('.', '\\.')\
+            .replace('-', '\\-')\
+            .replace('_', '\\_')
         url = f'https://api.telegram.org/bot{self.token}/sendMessage?chat_id={chat_id}&text={message}&parse_mode=MarkdownV2'
         response = requests.get(url)
         if response.status_code != 200:
-            raise Exception('Telegram Error - could not send message: {}'.format(response.text))
+            print('Telegram Error - could not send message: {}\n\n{}'.format(response.text, message), file=sys.stderr)
